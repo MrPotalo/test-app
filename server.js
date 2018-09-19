a4417356368ae30b7a6f60f8656672be4f5292b0
Malachi Grant <malachimgrant@gmail.com>
2018-09-18 21:17:25 -0300

    Added firebase connection

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const admin = require('firebase-admin');
 const port = process.env.PORT || 5000;
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    "projectId": process.env.FIREBASE_PROJECT_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "clientEmail": process.env.FIREBASE_CLIENT_EMAIL,
+  }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+});
+
+var db = admin.firestore();
+
+
 // API calls
 app.get('/api/hello', (req, res) => {
   res.send({ express: process.env.FIREBASE });

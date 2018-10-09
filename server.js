d4c91b063b6541923c7a2f8db37ad3ba381ddd84
Malachi Grant <malachimgrant@gmail.com>
2018-10-09 20:19:34 -0300

    Fixed Firebase connection and running heroku local.

--- server.js
+++ server.js
@@ -2,13 +2,20 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const admin = require('firebase-admin');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    "projectId": process.env.FIREBASE_PROJECT_ID,
+    "type": "service_account",
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
     "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-    "clientEmail": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_AUTH_URI,
+    "token_uri": process.env.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_CERT_URL
   }),
   databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
@@ -18,17 +25,15 @@ var db = admin.firestore();
 
 // API calls
 app.get('/api/hello', (req, res) => {
-  const test;
   db.collection('test').get()
     .then((snapshot) => {
       snapshot.forEach((doc) => {
-        console.log(doc.id, '=>', doc.data());
+        res.send(doc.data());
       });
     })
     .catch((err) => {
-      console.log('Error getting documents', err);
+      res.send(err);
     });
-  res.send({ express: test });
 });
 if (process.env.NODE_ENV === 'production') {
   // Serve any static files
